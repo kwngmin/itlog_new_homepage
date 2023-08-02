ea18c63a7251bd5c733a463836f53cc598fc7d55
kwngmin <kwngmink@gmail.com>
2023-08-02 18:00:42 +0900

    네이버 api 연동 테스트

--- src/js/company.js
+++ src/js/company.js
@@ -30,3 +30,11 @@ const aosChange = () => {
     });
   }
 };
+
+// 네이버 지도
+var mapOptions = {
+  center: new naver.maps.LatLng(37.3595704, 127.105399),
+  zoom: 10,
+};
+
+var map = new naver.maps.Map("map", mapOptions);
