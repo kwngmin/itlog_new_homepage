f8c8a425d7ecdfb0ee1b846ce0d770f87f778973
kwngmin <kwngmink@gmail.com>
2023-08-03 10:40:25 +0900

    주소 복사하기 추가

--- src/js/company.js
+++ src/js/company.js
@@ -18,7 +18,6 @@ window.addEventListener("resize", () => {});
 // 스크롤 하면 실행
 window.addEventListener("scroll", () => {
   if (window && window.scrollY > 100) {
-    scrollHeader();
     scrollTop();
   }
 });
@@ -30,11 +29,27 @@ const aosChange = () => {
     });
   }
 };
-
+// 주소 복사하기
+const copyAddress = () => {
+  // div 내부 텍스트 취득
+  const address = document.getElementById("address").innerText;
+  // textarea 생성
+  const textArea = document.createElement("textarea");
+  // textarea 추가
+  document.body.appendChild(textArea);
+  // textara의 value값으로 div내부 텍스트값 설정
+  textArea.value = address;
+  // textarea 선택 및 복사
+  textArea.select();
+  document.execCommand("copy");
+  // textarea 제거
+  document.body.removeChild(textArea);
+  console.log(address);
+  alert("주소가 복사되었습니다.");
+};
 // 네이버 지도
 var mapOptions = {
   center: new naver.maps.LatLng(37.3595704, 127.105399),
   zoom: 10,
 };
-
-var map = new naver.maps.Map("map", mapOptions);
+// var map = new naver.maps.Map("map", mapOptions);
